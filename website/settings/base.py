df3ce0664c8886e41f6cd3380effeb602c1ac2b1
Mary Kate Fain <mk@marykatefain.com>
2018-10-12 14:12:51 -0400

    try another ID

--- website/settings/base.py
+++ website/settings/base.py
@@ -166,4 +166,4 @@ WAGTAIL_SITE_NAME = "website"
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 BASE_URL = 'http://marykatefain.com'
 
-GA_VIEW_ID="ga:101750507"
+GA_VIEW_ID='ga:62506265'
