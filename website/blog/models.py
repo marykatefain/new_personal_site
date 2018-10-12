97a14fac8d77f063481889437e76bad2fbb70684
Mary Kate Fain <mk@marykatefain.com>
2018-10-12 17:21:34 -0400

    basic blog functionality, needs styling

--- website/blog/models.py
+++ website/blog/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
-from wagtail.core.fields import StreamField
+from wagtail.images.models import Image
+from wagtail.images.edit_handlers import ImageChooserPanel
+
+from wagtail.core.fields import StreamField, RichTextField
 from wagtail.core import blocks
 from wagtail.core.models import Page
 from wagtail.images.blocks import ImageChooserBlock
@@ -15,34 +18,32 @@ from wagtail.admin.edit_handlers import (
 
 
 class BlogIndexPage(Page):
-    body = StreamField([
-        ('content', blocks.RichTextBlock()),
-    ], null=True, blank=True)
-
-    sidebar = StreamField([
-        ('content', blocks.RichTextBlock()),
-    ], null=True, blank=True)
-
-    content_panels = Page.content_panels + [
-        StreamFieldPanel('body'),
-        StreamFieldPanel('sidebar'),
-    ]
 
     subpage_types = ['BlogPostPage']
 
     def get_context(self, request):
         context = super(BlogIndexPage, self).get_context(request)
-        context['blog_pages'] = self.get_children().type(BlogPostPage)
+        context['blog_pages'] = self.get_children().live().order_by('-first_published_at').specific
         return context
 
 
 class BlogPostPage(Page):
-    body = StreamField([
-        ('content', blocks.RichTextBlock()),
-    ], null=True, blank=True)
+    headline = models.CharField(max_length=255, blank=True, null=True)
+    subtitle = models.CharField(max_length=255, blank=True, null=True)
+    body = RichTextField(blank=True, null=True)
+    listing_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
 
     content_panels = Page.content_panels + [
-        StreamFieldPanel('body'),
+        FieldPanel('headline'),
+        FieldPanel('subtitle'),
+        FieldPanel('body'),
+        ImageChooserPanel('listing_image'),
     ]
 
     parent_page_types = ['BlogIndexPage']
